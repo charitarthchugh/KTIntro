678ba79b80677f3e1771548cd43632bf4e7175c4
charitarthchugh <37895518+charitarthchugh@users.noreply.github.com>
2020-03-24 21:51:35 -0400

    InitialCommit

---
+++ src/Hello.kt
@@ -0,0 +1,14 @@
+fun main(args:Array<String>){
+    var name:String="Yello";
+    // var declaration can be explicit or implicit
+    var includeName ="$name Mello";
+    //vars can be evaluated like above, within quotes(Spacing Resitrictions apply)
+    println(includeName);
+    //or like plain ol' Java, where the quotes are broken for strings
+    includeName=name+"Mello"
+    println(includeName);
+    /*
+    * Basic Math Functionality is SAME AS JAVA.
+    */
+
+}

1e1d73d8ab79407af74501dded60e60ab4a6a61d
charitarthchugh <37895518+charitarthchugh@users.noreply.github.com>
2020-03-29 12:40:52 -0400

    Add notes
    Add math, if else, arrays

---
+++ README.md
@@ -0,0 +1,3 @@
+# Welcome to Kotlin
+##This is for previous Java developers thatwant to learn basic Kotlin syntax
+Navigate to `! 
--- src/Hello.kt
+++ src/Hello.kt
@@ -1,14 +1,44 @@
+import java.util.Arrays;//Same As java
 fun main(args:Array<String>){
+    /*
+    *Kotlin does not need a semicolon- just go on to a new line
+    *Only use them when need 2 statements on the same line
+    */
+
     var name:String="Yello";
     // var declaration can be explicit or implicit
     var includeName ="$name Mello";
-    //vars can be evaluated like above, within quotes(Spacing Resitrictions apply)
+    //vars can be evaluated like above, within quotes (Spacing restrictions apply!)
     println(includeName);
     //or like plain ol' Java, where the quotes are broken for strings
     includeName=name+"Mello"
     println(includeName);
     /*
-    * Basic Math Functionality is SAME AS JAVA.
+    * Basic Math Functionality is SAME AS JAVA.(Including Shortcut operators)
+    * Default to Int(not lowercase int)
+    * then Double(for decimals
     */
+    var m:Int=2
+    m++
+    m+=3
+    println(m)
+    /*
+    * If else functionality SAME AS JAVA
+    */
+    if (m==2) {
+        println("true")
+    }
+    else
+        println(false)
+    /*
+    * Arrays are DIFFERENT though
+    * Kotlin has NATIVE ArrayList functionality
+    * ArrayLists cannot be used- but use mutableListOf to get that functionality
+    */
+    var arrays= listOf<Int>(1,2,3)//Immutable (can't change)
+    println(arrays)
+    var change= mutableListOf<Int>()
+    change.add(4)//Same as ArrayList
+    println(change)
 
 }
